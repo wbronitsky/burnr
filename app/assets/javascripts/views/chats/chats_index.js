ed63c7f96835ace1e936e081d2e4eb9af76f2adf
wbronitsky <wbronitsky@gmail.com>
2013-07-11 14:58:25 -0700

    Added burn to close of connection

--- app/assets/javascripts/views/chats/chats_index.js
+++ app/assets/javascripts/views/chats/chats_index.js
@@ -35,14 +35,14 @@ Chat.Views.ChatsIndex = Backbone.View.extend({
   submitBurn: function(event){
     var that = this;
     if (event.keyCode == 13){
-      that.burnrId = $('#burnrId').val();
+      this.burnrId = $('#burnrId').val();
       
       if (that.burnrId[0] == "/"){
-        that.burnrId = that.burnrId.slice(1);
+        this.burnrId = this.burnrId.slice(1);
       };
       
-      that.alias = $('#name').val();
-      that.myPeer = new Peer({key: 'p18l29bd9f11yvi'});
+      this.alias = $('#name').val();
+      this.myPeer = new Peer({key: 'p18l29bd9f11yvi'});
       
       if ($('#key').val() == "") {
         that.passPhrase = that.alias
@@ -50,12 +50,12 @@ Chat.Views.ChatsIndex = Backbone.View.extend({
         that.passPhrase = $('#key').val();
       };
 
-      that.yourRSAkey = cryptico.generateRSAKey(that.passPhrase, 1024)
-      that.yourPublicKeyString = cryptico.publicKeyString(that.yourRSAkey);
+      this.yourRSAkey = cryptico.generateRSAKey(that.passPhrase, 1024)
+      this.yourPublicKeyString = cryptico.publicKeyString(that.yourRSAkey);
 
       var conn = that.myPeer.connect(that.burnrId, {metadata: [that.alias, that.yourPublicKeyString]});
 
-      that.myPeer.on('open', function(){
+      this.myPeer.on('open', function(){
         console.log('thiers')
         $('.chatList').append('<li>please wait</li>')
         window.Chat.Store.conn = conn 
@@ -68,7 +68,7 @@ Chat.Views.ChatsIndex = Backbone.View.extend({
         });
       });
 
-      that.myPeer.on('error', function(){
+      this.myPeer.on('error', function(){
        console.log('mine')
         that.myPeer = new Peer(that.burnrId, {key: 'p18l29bd9f11yvi'})
         $('.chatList').append('<li>burnr initialized</li>')
@@ -95,18 +95,22 @@ Chat.Views.ChatsIndex = Backbone.View.extend({
       $('.chatHead').append('/'+that.burnrId)
       $('.chatHead').append("<button class='burn'>burn</button>")
       
-      that.myPeer.on('close', function(){
+      this.myPeer.on('close', function(){
         that.burn();
-      })
+      });
+
+      window.Chat.Store.conn.on('close', function(){
+        that.burn();
+      });
     }
   },
 
   burn: function(){
     $('.chatList').empty();
-    $('.chatHead').empty()
-    $('.chatHead').append('<input name="burnr" placeholder="burnrId" id="burnrId">')
-    $('.chatHead').append(' <input name="name" placeholder="alias" id="name">')
-    $('.chatHead').append(' <input name="key" placeholder="encryption key" id="key">')
+    $('.chatHead').empty();
+    $('.chatHead').append('<input name="burnr" placeholder="burnrId" id="burnrId">');
+    $('.chatHead').append(' <input name="name" placeholder="alias" id="name">');
+    $('.chatHead').append(' <input name="key" placeholder="encryption key" id="key">');
 
     window.Chat.Store.conn.close();
     this.myPeer.disconnect();
